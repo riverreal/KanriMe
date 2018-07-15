a5c4db1b01d9fa52257894fe34b852eeb6bf3149
Rivero Sato <riveroyuichitrabajosdeinforma@gmail.com>
2018-07-15 22:28:46 +0900

    Adjust application rendering for lower consumption

--- Assets/Scripts/KanriMe.cs
+++ Assets/Scripts/KanriMe.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KanriMe : MonoBehaviour {
-	
-	// Use this for initialization
-	void Start () {
-		
+public class KanriMe : MonoBehaviour
+{
+	public static KanriMe m_instance;
+	public static KanriMe Instance
+	{
+		get{
+			if(m_instance == null)
+			{
+				m_instance = GameObject.FindObjectOfType<KanriMe>();
+
+				if(m_instance == null)
+				{
+					GameObject container = new GameObject("KanriMe");
+					m_instance = container.AddComponent<KanriMe>();
+				}
+			}
+
+			return m_instance;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Awake()
+	{
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 15;
 	}
 }
